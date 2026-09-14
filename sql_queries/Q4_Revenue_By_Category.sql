8d04e02a945de20b1184003f5f921bcaf0ea7a80
Ian Yes Dalawampu <ianyesdalawampu@gmail.com>
2026-09-14 13:12:28 +0800

    Refactor SQL joins from inner to standard join

--- sql_queries/Q4_Revenue_By_Category.sql
+++ sql_queries/Q4_Revenue_By_Category.sql
@@ -9,9 +9,9 @@ select
     p.product_category_name,
     round(sum(op.payment_value),2) as total_revenue
 from order_payments op
-inner join order_items oi 
+join order_items oi 
     on oi.order_id = op.order_id
-inner join products p 
+join products p 
     on p.product_id = oi.product_id
 group by p.product_category_name
 order by total_revenue desc;
