a8a80b38bfbdaf18bef5eb2177fd52fa56fd72da
Ian Yes Dalawampu <ianyesdalawampu@gmail.com>
2026-09-14 12:42:18 +0800

    Add SQL solution for Q1 Top Customers

---
+++ sql_queries/Q1_Top_Customers.sql
@@ -0,0 +1,22 @@
+-- ===========================================
+-- Q1: Top 10 Customers by Total Amount Spent
+-- Objective:
+-- Identify the customers with the highest total payment value.
+-- ===========================================
+
+with customer_spending as (
+    select o.customer_id,
+            sum(op.payment_value) as total_spent
+    from orders o
+    inner join order_payments op
+        on o.order_id = op.order_id
+    group by o.customer_id
+    )
+
+select c.customer_unique_id,
+       cs.total_spent
+from customers c
+inner join customer_spending cs
+    on c.customer_id = cs.customer_id
+order by total_spent desc
+limit 10;
